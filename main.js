39ebbf67249e91862cc72d25b7e939a31dd110b7
Th4Dude <gallottimaurizio@yahoo.it>
2023-03-07 16:54:54 +0200

    creazione card+img

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 'use strict'
 
+/* array con oggetti */
+
 const membriDelTeam =
     [{
         nome: 'Wayne Barnett',
@@ -15,30 +17,27 @@ const membriDelTeam =
         nome: 'Walter Gordon',
         ruolo: 'Office Manager',
         foto: 'walter-gordon-office-manager.jpg'
-
     },
     {
         nome: 'Angela Lopez',
         ruolo: 'Social Media Manager',
         foto: 'angela-lopez-social-media-manager.jpg'
-
     },
     {
         nome: 'Scott Estrada',
         ruolo: 'Developer',
         foto: 'scott-estrada-developer.jpg'
-
     },
     {
         nome: 'Barbara Ramos',
         ruolo: 'Graphic Designer',
         foto: 'barbara-ramos-graphic-designer.jpg',
-
     }];
 
+    /* array con oggetti */
 
+    /* variabili per ogni user */
 
-    
 const userTeamUno = {
     nome: 'Wayne Barnett',
     ruolo: 'Founfed & CEO',
@@ -74,5 +73,7 @@ const userTeamSei = {
     ruolo: 'Graphic Designer',
     foto: 'barbara-ramos-graphic-designer.jpg',
 }
+
+/* variabili per ogni user */
 
 console.log(membriDelTeam);
