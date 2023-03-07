4ab030871d8a1d90fd9d34fce112bb158f40cad5
Th4Dude <gallottimaurizio@yahoo.it>
2023-03-07 15:49:20 +0200

    milestone 0-1 done

--- main.js
+++ main.js
@@ -1,2 +1,78 @@
 'use strict'
 
+const membriDelTeam =
+    [{
+        nome: 'Wayne Barnett',
+        ruolo: 'Founfed & CEO',
+        foto: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        nome: 'Wayne Barnett',
+        ruolo: 'Founfed & CEO',
+        foto: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        nome: 'Walter Gordon',
+        ruolo: 'Office Manager',
+        foto: 'walter-gordon-office-manager.jpg'
+
+    },
+    {
+        nome: 'Angela Lopez',
+        ruolo: 'Social Media Manager',
+        foto: 'angela-lopez-social-media-manager.jpg'
+
+    },
+    {
+        nome: 'Scott Estrada',
+        ruolo: 'Developer',
+        foto: 'scott-estrada-developer.jpg'
+
+    },
+    {
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic Designer',
+        foto: 'barbara-ramos-graphic-designer.jpg',
+
+    }];
+
+
+
+    
+const userTeamUno = {
+    nome: 'Wayne Barnett',
+    ruolo: 'Founfed & CEO',
+    foto: 'wayne-barnett-founder-ceo.jpg',
+}
+
+const userTeamDue = {
+    nome: 'Angela Caroll',
+    ruolo: 'Chief Editor',
+    foto: 'angela-caroll-chief-editor.jpg',
+}
+
+const userTeamTre = {
+    nome: 'Walter Gordon',
+    ruolo: 'Office Manager',
+    foto: 'walter-gordon-office-manager.jpg',
+}
+
+const userTeamQuattro = {
+    nome: 'Angela Lopez',
+    ruolo: 'Social Media Manager',
+    foto: 'angela-lopez-social-media-manager.jpg',
+}
+
+const userTeamCinque = {
+    nome: 'Scott Estrada',
+    ruolo: 'Developer',
+    foto: 'scott-estrada-developer.jpg',
+}
+
+const userTeamSei = {
+    nome: 'Barbara Ramos',
+    ruolo: 'Graphic Designer',
+    foto: 'barbara-ramos-graphic-designer.jpg',
+}
+
+console.log(membriDelTeam);
--- readme.md
+++ readme.md
@@ -8,20 +8,25 @@ foto
 MILESTONE 0:
 Creare l’array di oggetti con le informazioni fornite.
 
+//creo un array contenente tutte le info del singolo membro
+
+
+
 MILESTONE 1:
 Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 
 MILESTONE 2:
 Stampare le stesse informazioni su DOM sottoforma di stringhe
 
-BONUS 1:
+<!-- BONUS 1:
 Trasformare la stringa foto in una immagine effettiva
+
 BONUS 2:
 Organizzare i singoli membri in card/schede
 Consigli del giorno:
 Ragioniamo come sempre a step.
 Prima la logica in italiano e poi traduciamo in codice.
-E ricordiamoci che console.log() è nostro amico!
+E ricordiamoci che console.log() è nostro amico! -->
 
 
 
